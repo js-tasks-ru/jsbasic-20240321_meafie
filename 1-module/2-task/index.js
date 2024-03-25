9542ec469b6872768879eb15823ac61b7d9bf89f
Ivan Romanov <rmnv.ivn@gmail.com>
2024-03-25 20:44:39 +0400

    2nd task of module 1 completed and tested

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,15 +10,19 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name && name.length > 3 && name.indexOf(" ") === -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
