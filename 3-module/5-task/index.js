65ef687bf8e1e2fb95e1672dd538994b2a4eb2e9
Ivan Romanov <rmnv.ivn@gmail.com>
2024-04-02 12:17:02 +0400

    5th task of module 3 completed and tested

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let sorted = str
+    .split(" ")
+    .filter((item) => Number(item))
+    .sort((a, b) => a - b);
+  return {
+    min: +sorted[0],
+    max: +sorted.at(-1),
+  };
 }
