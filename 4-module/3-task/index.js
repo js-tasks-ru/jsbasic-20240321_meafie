faca26c24f684126fa97a01efbed5af5a90c7455
Ivan Romanov <rmnv.ivn@gmail.com>
2024-04-06 11:37:31 +0400

    3d task of module 4 completed and tested

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
 function highlight(table) {
-  // ваш код...
+  const rows = Array.from(table.rows);
+  rows.forEach((row) => {
+    if (row.cells[3].dataset.available) {
+      if (row.cells[3].dataset.available === "true") {
+        row.classList.add("available");
+      } else {
+        row.classList.add("unavailable");
+      }
+    } else {
+      row.setAttribute("hidden", "");
+    }
+    if (row.cells[2].textContent === "m") {
+      row.classList.add("male");
+    } else if (row.cells[2].textContent === "f") {
+      row.classList.add("female");
+    }
+    if (+row.cells[1].textContent < 18) {
+      row.style = "text-decoration: line-through";
+    }
+  });
 }
