3c589240b20b7bf347b4e873c6cdea588d415282
Ivan Romanov <rmnv.ivn@gmail.com>
2024-04-06 10:41:36 +0400

    1st task of module 4 completed and tested

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,7 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const list = document.createElement("ul");
+  friends.forEach((item) => {
+    list.innerHTML += `<li>${item.firstName} ${item.lastName}</li>`;
+  });
+  return list;
 }
